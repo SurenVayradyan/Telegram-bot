e88c6d67de42da1ab135fc0db889a38154953700
Сурен Вайрадян <98884204+SurenVayradyan@users.noreply.github.com>
2022-04-28 11:26:12 +0300

    Финал
    переделал остальные функции

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(token)
 
 tasks = {}
 
-random_tasks = ['позвонить бабушке', 'позвонить маме', 'посмотреть фильм'
+random_tasks = ['позвонить бабушке', 'позвонить маме', 'посмотреть фильм', 'поиграть в баскетбол'
 ]
 
 def add(date, task):
@@ -27,38 +27,6 @@ HELP = """
 /random - добавлять случайную задачу на дату Сегодня
 """
 
-random_tasks = ['Позвонить бабушке', 'Позвонить маме', 'Посмотреть фильм'
-]
-
-tasks = {
-
-    }
-
-run = True
-
-today = {
-
-}
-tomorrow = {
-
-}
-other = {
-
-}
-
-def add(date, task):
-    if date in tasks:
-        # Дата есть в словаре
-        # Добавляем в список задачу
-        tasks[date].append(task)
-    else:
-        # Даты нет в словаре
-        # Создаем записить с ключом date
-        tasks[date] = []
-        tasks[date].append(task)
-    print('Задача:', task, '\n''Добавлена на дату:', date)
-
-    
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, HELP)
@@ -69,44 +37,26 @@ def add_todo(message):
     add(command[1].lower(), command[2])
     bot.send_message(message.chat.id, 'Задача: ' + command[2] + '\n' + 'Добавлена на дату: ' + command[1].lower())
 
-# while run:
-#     command = input('Введите команду: ')
-#     if command == 'help':
-#         print(HELP)
-#     elif command == 'show':
-#         date = input("Введите дату для отображения списка задач на этот день: ")
-#         if date in tasks:
-#             for task in tasks[date]:
-#                 print('-', task)
-#         else:
-#             print('Такой даты нет')
-#             print('Попробуйте ввести другую дату')
+@bot.message_handler(commands=['random'])
+def random_add(message):
+    date = 'сегодня'
+    task = random.choice(random_tasks)
+    add(date, task)
+    text = 'Задача: ' + task + '\n' + 'Добавлена на дату: ' + date
+    bot.send_message(message.chat.id, text)
 
-#     elif command == 'add':
-#         date = input('Введите дату для добавления задачи: ')
-#         task = input('Введите какую задачу хотите добавить в список дел: ')
-#         complite = input('Введите дату для выполнения задачи: ')
-#         add(date, task)
-#         if complite == 'Сегодня':
-#             today[complite] = []
-#             today[complite].append(complite)
-#         elif complite == 'Завтра':
-#             tomorrow[complite] = []
-#             tomorrow[complite].append(complite)
-#         else:
-#             other[complite] = []
-#             other[complite].append(complite)
+@bot.message_handler(commands=['show', 'print'])
+def show(message):
+    command = message.text.split(maxsplit=1)
+    date = command[1].lower()
+    text = ''
+    if date in tasks:
+        text = date.upper() + '\n'
+        for task in tasks[date]:
+            text = text + '[]' + task + '\n'
+    else:
+        text = 'Задач на эту дату нет'
+    bot.send_message(message.chat.id, text)
 
-#     elif command == 'random':
-#         task = random.choice(random_tasks)
-#         add('Сегодня',task)
-
-#     elif command == 'exit':
-#         print('Спасибо за использование! До свидания!')
-#         break
-
-#     else:
-#         print('Неизвестная команда')
-#         print('До свидания!')
-#         break
-
+# Постоянно обращается к серверам телеграм
+bot.polling(none_stop=True) # отправка к серверам телеграмм используя наш токен
